ae3d586df80e240b4051d771bdd7d20ad3e6bbc5
abdullox <abdulloxislomov39@gmail.com>
2024-07-29 18:16:17 +0500

    edit

--- config/settings.py
+++ config/settings.py
@@ -80,6 +80,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+CSRF_TRUSTED_ORIGINS = ['http://noyob.mebel-zakaz.uz']
+
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
